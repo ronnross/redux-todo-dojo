eeff4893e625c103ee0b7249056fe9b8a86d302f
Ronn Ross <ronn.ross@gmail.com>
2015-09-28 10:27:26 -0400

    added redux dev-tools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+// Redux utility functions
+import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+
+// Redux DevTools store enhancers
+import { devTools, persistState } from 'redux-devtools';
+// React components for Redux DevTools
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 import App from './containers/App';
 import todoApp from './reducers';
-import storeConfig from './store';
 
-let store = createStore(todoApp);
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+const store = finalCreateStore(todoApp);
+
+if (module.hot) {
+  module.hot.accept('./reducers', () =>
+    store.replaceReducer(combineReducers(require('./reducers')))
+  );
+}
+
+export default class Root extends Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <App />
+        </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store}
+                    monitor={LogMonitor}
+                    visibleOnLoad={false} />
+        </DebugPanel>
+      </div>
+    );
+  }
+}
 
 let rootElement = document.getElementById('main');
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root />,
   rootElement
 );
